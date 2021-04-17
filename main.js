e8b4b09201e306856b2d74fa3da100ededa77ba0
Yecid Leyes <yecidleyesb0321@gmail.com>
2021-04-17 12:58:49 -0500

    Continuamos trabajando

--- main.js
+++ main.js
@@ -85,3 +85,27 @@ verAbajo.addEventListener('click' , () => {
 })
 //FIN BOTONES IR ARRIBA Y MOSTRAR ABAJO//
 //FIN EFECTO SCROLL//
+
+//PARA AGREGAR O QUITAR LA BARS
+function listo(){
+    jQuery(".bars").click(function(e){
+      e.preventDefault();
+      //AGREGA O QUITA EL OPEN
+      jQuery("header .container nav").toggleClass("open");
+      //AGREGA O QUITA LA X
+      jQuery(".bars i").toggleClass("fa-times");
+    });
+    //ELEMENTOS a DEL NAV
+    jQuery("header .container nav a").click(function(){
+      //PARA REMOVER EL MENU X
+      jQuery("header .container nav").removeClass("open");
+      jQuery(".bars i").removeClass("fa-times");
+      //FIN REMOVER MENU X
+  
+      var col = jQuery(this).attr("href");
+      //PARA QUE SE MUEVA HASTA LA POSICION "a"
+      jQuery("html,body").animate({
+        "scrollTop": jQuery(col).offset().top -99
+      })
+    });
+  }
