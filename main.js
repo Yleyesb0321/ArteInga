415415f2d05917240e1671fc695f43b6f5c2d6b3
Yecid Leyes <yecidleyesb0321@gmail.com>
2021-04-19 12:45:58 -0500

    Completamos la pagina

--- main.js
+++ main.js
@@ -5,40 +5,6 @@ let containerMenu = document.querySelector(".menu");
 let activador = true;
 //FIN VARIABLES//
 
-//DETECTAR CLICK BTN MENU//
-btnMenu.addEventListener("click", (event) => {
-
-    document.querySelector('.btn-menu i').classList.toggle('fa-times');
-    
-    if (activador) {
-        menu.style.left = '0%';
-        menu.style.transition = '0.5s';
-
-        activador = false;
-    }
-    else {
-        activador = false;
-        menu.style.left = '-100%';
-        
-        activador = true
-    }
-})
-//FIN DETECTAR CLICK BTN MENU//
-
-//MENU ACTIVO//
-let enlaces = document.querySelectorAll('.lists li a');
-
-enlaces.forEach( (element) =>{
-    element.addEventListener('click', (event) => {
-        enlaces.forEach( (link) => {
-            link.classList.remove('activo');
-        });
-        event.target.classList.add('activo');
-
-    });
-})
-//FIN MENU ACTIVO//
-
 //EFECTO SCROLL//
 let prevScrollPos = window.pageYOffset;
 let goTop = document.querySelector('.go-top');
